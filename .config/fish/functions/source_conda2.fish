ad34000f9c3ffa6336c9ab3de73074616c3cfb14
Miguel C <mkarmona@gmail.com>
2018-12-14 19:18:05 +0000

    refactor: new updates and prompt colors

---
+++ .config/fish/functions/source_conda2.fish
@@ -0,0 +1,3 @@
+function source_conda2
+    source /home/mkarmona/opt/anaconda2/etc/fish/conf.d/conda.fish
+end
---
+++ .config/fish/functions/source_conda3.fish
@@ -0,0 +1,3 @@
+function source_conda3
+    source /home/mkarmona/opt/anaconda3/etc/fish/conf.d/conda.fish
+end
