7b1a1e776add257b9baa282e80503c2890f069c5
Miguel C <mkarmona@gmail.com>
2019-03-21 13:43:31 +0000

    new script for fullhd scale

---
+++ bin/scale-work-hd.sh
@@ -0,0 +1,3 @@
+xrandr --output DP1 --scale 1.6x1.6 --panning 3072x1728+2560+0
+#xrandr --output DP1 --scale 1.6x1.6 --panning 3072x1920+2560+0
+#xrandr --output DP1 --scale 1.7x1.7 --panning 3264x2040+2560+0
